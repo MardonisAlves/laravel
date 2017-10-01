0d92ceffae35b1d67650d5f740c15780058bb4e0
Mardonis Alves <mardonisgp@gmail.com>
2017-10-01 15:20:15 -0300

    mneur sidebar

--- resources/views/layouts/navbarHome.blade.php
+++ resources/views/layouts/navbarHome.blade.php
@@ -34,7 +34,9 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
+					@if(Auth::guest())
 					<li><a href="{{ url('/') }}">Home</a></li>
+					@else
 					<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Cadastros<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -53,12 +55,9 @@
 							</ul>
 					</li>
 				</ul>
-
+				@endif
 				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
+					@if(Auth::user())
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
